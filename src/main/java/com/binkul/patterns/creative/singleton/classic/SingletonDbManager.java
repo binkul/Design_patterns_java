041627b49b9fc4b36991d7ab7e73386dffa362d0
binkul <jacekbinkul@gmail.com>
2022-11-11 11:12:58 +0100

    Singleton

---
+++ src/main/java/com/binkul/patterns/creative/singleton/classic/SingletonDbManager.java
@@ -0,0 +1,55 @@
+package com.binkul.patterns.creative.singleton.classic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * Call it as:
+ * Connection conn = SingletonDbManager.getInstance().getConnection()
+ * Statement statement = conn.createStatement();
+ * String sql = "Select count(*) as amount From users";
+ * ResultSet set = statement.executeQuery(sql);
+ * setCount.next();
+ * int count = setCount.getInt("amount");
+ * setCount.close();
+ * <p>
+ * sql = "Select * From users";
+ * set = statement.executeQuery(sql);
+ * while (set.next()) {
+ *      System.out.println(set.getInt("ID") + ", " + set.getString("FIRSTNAME") + ", " + set.getString("LASTNAME"));
+ * }
+ * set.close();
+ * statement.close();
+ *
+ */
+
+public class SingletonDbManager {
+    private final Connection conn;
+    private static SingletonDbManager singletonDbManager = null;
+
+    private SingletonDbManager() throws SQLException {
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", "user");
+        connectionProperties.put("password", "password");
+        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_name?serverTimezone=Europe/Warsaw&useSSL=false" +
+                "&allowPublicKeyRetrieval=true", connectionProperties);
+    }
+
+    public static SingletonDbManager getInstance() throws SQLException {
+        if (singletonDbManager == null) {
+            synchronized (SingletonDbManager.class) {
+                if (singletonDbManager == null) {
+                    singletonDbManager = new SingletonDbManager();
+                }
+            }
+        }
+        return singletonDbManager;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+}
---
+++ src/main/java/com/binkul/patterns/creative/singleton/enumtype/SingletonDbManager.java
@@ -0,0 +1,56 @@
+package com.binkul.patterns.creative.singleton.enumtype;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ *
+ * Call it as:
+ * Connection conn = SingletonDbManager.INSTANCE.getConnection()
+ * Statement statement = conn.createStatement();
+ * String sql = "Select count(*) as amount From users";
+ * ResultSet set = statement.executeQuery(sql);
+ * setCount.next();
+ * int count = setCount.getInt("amount");
+ * setCount.close();
+ * <p>
+ * sql = "Select * From users";
+ * set = statement.executeQuery(sql);
+ * while (set.next()) {
+ *      System.out.println(set.getInt("ID") + ", " + set.getString("FIRSTNAME") + ", " + set.getString("LASTNAME"));
+ * }
+ * set.close();
+ * statement.close();
+ *
+ */
+
+public enum SingletonDbManager {
+    INSTANCE;
+
+    private Connection connection;
+    private String status;
+
+    SingletonDbManager() {
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", "user");
+        connectionProperties.put("password", "password");
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/DB_name?serverTimezone=Europe/Warsaw&useSSL=false" +
+                    "&allowPublicKeyRetrieval=true", connectionProperties);
+            status = "Connection established ...";
+        } catch (SQLException ex) {
+            connection = null;
+            status = "Connection is failed.";
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
